e491330c4b1df13084e15c8311c0fdbfb37eda43
NewbieCoderMorocco <82718864+NewbieCoderMorocco@users.noreply.github.com>
2022-07-19 10:31:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".btn"),
+  search = document.querySelector(".search"),
+  input = document.querySelector(".input");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
